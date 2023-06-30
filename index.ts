3d814fb45e80130723c8a497a8f6668b1096223e
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-06-30 17:05:10 -0400

    chore: first stable version

---
+++ index.ts
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+import { TextDocument } from 'vscode-languageserver-textdocument'
+import {
+  ProposedFeatures,
+  TextDocumentSyncKind,
+  TextDocuments,
+  createConnection,
+} from 'vscode-languageserver/node'
+import {
+  VSCodeEmmetConfig,
+  doComplete,
+  getEmmetMode,
+} from '@vscode/emmet-helper'
+
+const connection = createConnection(ProposedFeatures.all)
+const documents = new TextDocuments(TextDocument)
+
+let globalConfig: VSCodeEmmetConfig = {}
+
+connection.onInitialize(params => {
+  globalConfig = params.initializationOptions || {}
+
+  return {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+      completionProvider: {
+        resolveProvider: true,
+      },
+    },
+  }
+})
+
+connection.onCompletion(textDocumentPosition => {
+  const document = documents.get(textDocumentPosition.textDocument.uri)
+
+  if (!document) {
+    return
+  }
+
+  const languageId = document.languageId
+  const syntax = getEmmetMode(languageId)
+
+  if (!syntax) {
+    return
+  }
+
+  const position = textDocumentPosition.position
+
+  return doComplete(document, position, syntax, globalConfig)
+})
+
+documents.listen(connection)
+connection.listen()
