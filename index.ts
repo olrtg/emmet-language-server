26ff562ae47a909d7a53046a6ebebad23e201460
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-07-31 21:21:56 -0400

    feat: improve completion triggering

--- index.ts
+++ index.ts
@@ -26,6 +26,64 @@ connection.onInitialize(params => {
       textDocumentSync: TextDocumentSyncKind.Incremental,
       completionProvider: {
         resolveProvider: false,
+        triggerCharacters: [
+          // NOTE: For cases where is valid to expand emmet abbreviations with
+          // special characters
+          '!', // eg. `!` and `!!!` snippets in html or `!important` in css
+          ':', // eg. `w:` should expand to `width: |;`
+          '>', // https://docs.emmet.io/abbreviations/syntax/#child-gt
+          '+', // https://docs.emmet.io/abbreviations/syntax/#sibling
+          '^', // https://docs.emmet.io/abbreviations/syntax/#climb-up
+          '*', // https://docs.emmet.io/abbreviations/syntax/#multiplication
+          ')', // https://docs.emmet.io/abbreviations/syntax/#grouping
+          '.', // https://docs.emmet.io/abbreviations/syntax/#id-and-class
+          ']', // https://docs.emmet.io/abbreviations/syntax/#custom-attributes
+          '@', // https://docs.emmet.io/abbreviations/syntax/#changing-numbering-base-and-direction
+          '}', // https://docs.emmet.io/abbreviations/syntax/#text
+
+          // NOTE: For cases where completion is not triggered by typing a
+          // single character
+          'a',
+          'b',
+          'c',
+          'd',
+          'e',
+          'f',
+          'g',
+          'h',
+          'i',
+          'j',
+          'k',
+          'l',
+          'm',
+          'n',
+          'o',
+          'p',
+          'q',
+          'r',
+          's',
+          't',
+          'u',
+          'v',
+          'w',
+          'x',
+          'y',
+          'z',
+
+          // NOTE: For cases where completion is not triggered by typing a
+          // single character or because numbers cannot be used to trigger
+          // completion
+          '0',
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          '7',
+          '8',
+          '9',
+        ],
       },
     },
   }
